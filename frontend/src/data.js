d1c35d51b5dc567198c3cc2e20c4fbc006ad3c2b
ABDELBAKI KBABRA <abdelbakikbabra@gmail.com>
2021-09-24 23:18:15 +0100

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,65 @@
+export const data={
+    products:[
+        {
+            _id:'1',
+            name:'Cage',
+            category:'Cages',
+            image:'/images/doggy.jpg',
+            price:120,
+            brand:'Maart',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality'
+            
+        },
+        {
+            _id: '2',
+            name: 'Toy',
+            category: 'Toys',
+            image: '/images/toy.jpg',
+            price: 250,
+            brand: 'W&B',
+            rating: 5,
+            numReviews: 20,
+            description: 'high quality toy'
+
+        },
+        {
+            _id: '3',
+            name: 'Chick',
+            category: 'cloths',
+            image: '/images/blanket.jpg',
+            price: 520,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 14,
+            description: 'high quality blanket'
+
+        },
+        {
+            _id: '4',
+            name: 'makla dialhum',
+            category: 'food',
+            image: '/images/makla.jpg',
+            price: 410,
+            brand: 'Felix',
+            rating: 4,
+            numReviews: 50,
+            description: 'high quality food'
+
+        },
+        {
+            _id: '5',
+            name: 'maart',
+            category: 'maart',
+            image: '/images/toy2.jpg',
+            price: 200,
+            brand: 'chi haja',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality'
+
+        },
+        
+    ],
+};
